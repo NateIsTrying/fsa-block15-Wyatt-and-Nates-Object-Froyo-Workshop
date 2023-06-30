fb117f8aa0b91d3f0e86baf70adb22032a92c3a3
Nathan Kim <nastynate@pop-os.localdomain>
2023-06-30 15:07:17 -0400

    initial commit; half finished the project then handed it to Wyatt

---
+++ froyo.js
@@ -0,0 +1,24 @@
+const survey = {
+    firstName: 'jake',
+    lastName: 'smith',
+    email: 'jakesmith!aol.com',
+    phone: undefined,
+    zipCode: 631,
+    favoriteFlavors: 32,
+    cupSize: 'medium',
+    favoriteStore: 'Target',
+    firstVisit: false,
+}
+
+survey['email'] = 'jak3Smith1992@gmail.com';
+console.log(survey['email']);
+
+survey['phone'] = 3195551234;
+console.log(survey['phone']);
+
+survey['zipCode'] = '63132';
+console.log(survey['zipCode']);
+
+survey['favoriteFlavors'] = ["coffee", "strawberry", "matcha"];
+console.log(survey['favoriteFlavors']);
+
